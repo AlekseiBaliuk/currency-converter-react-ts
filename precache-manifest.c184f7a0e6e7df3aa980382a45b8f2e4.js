47614045e649af5ff9f5c8bf9dbedbf7e6a22cb9
AlekseiBaliuk <aleksred.b@gmail.com>
2022-12-16 11:59:51 +0000

    Deploying to gh-pages from @ AlekseiBaliuk/currency-converter-react-ts@79932130efb884c96da8e2ea2f85c222e572100b 🚀

---
+++ precache-manifest.c184f7a0e6e7df3aa980382a45b8f2e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc724f61555d3346bef0",
+    "url": "/currency-converter-react-ts/static/css/main.f6fa6055.chunk.css"
+  },
+  {
+    "revision": "cc724f61555d3346bef0",
+    "url": "/currency-converter-react-ts/static/js/main.924418d3.chunk.js"
+  },
+  {
+    "revision": "023c23208a73fc5ac91c",
+    "url": "/currency-converter-react-ts/static/js/runtime~main.4b6e524e.js"
+  },
+  {
+    "revision": "fc0112cd48593e052b64",
+    "url": "/currency-converter-react-ts/static/js/2.c50ae168.chunk.js"
+  },
+  {
+    "revision": "28e9780c9be4a84337c74b28748618b8",
+    "url": "/currency-converter-react-ts/index.html"
+  }
+];
